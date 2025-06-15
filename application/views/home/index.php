548b57a443480865ec2625de45d9a5866e5eee61
jabjabrik <yusufjabriko@gmail.com>
2025-06-15 20:00:53 +0700

    update link

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -31,7 +31,7 @@
 <body class="index-page">
     <header id="header" class="header d-flex align-items-center sticky-top">
         <div class="container position-relative d-flex align-items-center">
-            <a href="index.html" class="logo d-flex align-items-center me-auto">
+            <a href="<?= base_url(); ?>" class="logo d-flex align-items-center me-auto">
                 <h1 class="sitename">SiKartuKerja</h1>
             </a>
             <nav id="navmenu" class="navmenu">
